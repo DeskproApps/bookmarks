62e4d5525a5fd97851f9de77511bcfa6a5b9b14f
Tweeno <itweeno@gmail.com>
2023-03-08 12:05:30 +0000

    fixed font

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 
+import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
