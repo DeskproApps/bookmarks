b925dd908683dba98e1983bedddf18007af80f6d
ilia makarov <ilia.makarov@me.com>
2024-06-04 15:09:25 +0300

    Removed iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
-import "iframe-resizer/js/iframeResizer.contentWindow.js";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import { BookmarkProvider } from "./context/bookmarkContext";
 
