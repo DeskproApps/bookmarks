212b0e69190de6101e12e14b0a5353d4657e1fda
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-08-21 11:35:55 +0100

    [Feature] SC-124017 Refactor apps to remove iframe-resizer dependency and make iframe content sizing more robust

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,18 @@ import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import { BookmarkProvider } from "./context/bookmarkContext";
+import "simplebar/dist/simplebar.min.css";
+import { Scrollbar } from "@deskpro/deskpro-ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
   <React.StrictMode>
-    <DeskproAppProvider>
-      <BookmarkProvider>
-        <App />
-      </BookmarkProvider>
-    </DeskproAppProvider>
+    <Scrollbar style={{ height: "100%", width: "100%" }}>
+      <DeskproAppProvider>
+        <BookmarkProvider>
+          <App />
+        </BookmarkProvider>
+      </DeskproAppProvider>
+    </Scrollbar>
   </React.StrictMode>
 );
