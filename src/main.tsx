1346d165e2f6c9e7737afb53917c99908877d535
Paul Happy Hutchinson <paul.hutchinson@deskpro.com>
2024-08-28 16:42:51 +0100

    SC-159043 Teams integration - agent authentication via Vendor OAuth2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import { BookmarkProvider } from "./context/bookmarkContext";
 import "simplebar/dist/simplebar.min.css";
-import { Scrollbar } from "@deskpro/deskpro-ui";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
