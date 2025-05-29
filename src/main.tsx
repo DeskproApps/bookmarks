8ed43ef9c0979a66dc71f914d6a23dd0d343bc90
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-05-29 11:54:24 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#48)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './instrument';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
@@ -6,8 +7,11 @@ import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "@deskpro/deskpro-ui/dist/deskpro-ui.css";
 import { BookmarkProvider } from "./context/bookmarkContext";
 import "simplebar/dist/simplebar.min.css";
+import { reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render(
   <React.StrictMode>
     <div style={{ height: "100%", width: "100%" }}>
