3cec50e875cefe6ffd616e8a95d7a5ba7356978c
OJ Abba <phavorabba@gmail.com>
2025-07-16 11:10:20 +0100

    [Chore] Update theme typings for styled components

--- src/styles.ts
+++ src/styles.ts
@@ -1,9 +1,10 @@
+import { DeskproAppTheme } from "@deskpro/app-sdk";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const StyledLink = styled(Link)`
+export const StyledLink = styled(Link)<DeskproAppTheme>`
   all: unset;
   color: ${({ theme, to }) =>
-    to ? theme.colors.cyan100 : theme.colors.black100};
+    to ? theme.colors.cyan100 : theme.colors.grey100};
   cursor: ${({ to }) => (to ? "pointer" : "default")};
 `;
